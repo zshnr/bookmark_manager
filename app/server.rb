f502996c8d027d5318e2e9d7c369afd3c643b054
Zeeshan Rasool <zrasool88@gmail.com>
2014-10-07 18:59:59 +0100

    added password confirmation

--- app/server.rb
+++ app/server.rb
@@ -33,7 +33,8 @@ set :session_secret, 'my unique encryption key!'
 	end
 
 	post '/users' do
-		user = User.create(:email => params[:email], :password => params[:password])
+		user = User.create(:email => params[:email], :password => params[:password], 
+			:password_confirmation => params[:password_confirmation])
 		session[:user_id] = user.id
 		redirect to('/')
 	end
