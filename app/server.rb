fc07692767758af17af4c7e4b2ad137091bbc6ef
Zeeshan Rasool <zrasool88@gmail.com>
2014-10-07 19:40:59 +0100

    preventing duplicate registrations

--- app/server.rb
+++ app/server.rb
@@ -1,5 +1,6 @@
 require 'data_mapper' 
 require 'sinatra'
+require 'rack-flash'
 require './lib/link'
 require './lib/tag'
 require './lib/user'
@@ -8,6 +9,7 @@ require_relative 'data_mapper_setup'
 
 enable :sessions
 set :session_secret, 'my unique encryption key!'
+use Rack::Flash
 
 	get '/' do
    	 	@links = Link.all
@@ -29,12 +31,18 @@ set :session_secret, 'my unique encryption key!'
 	end
 
 	get '/users/new' do
+		@user = User.new
 		erb :"users/new"
 	end
 
 	post '/users' do
-		user = User.create(:email => params[:email], :password => params[:password], 
-			:password_confirmation => params[:password_confirmation])
-		session[:user_id] = user.id
-		redirect to('/')
+		@user = User.new(:email => params[:email], :password => params[:password], 
+						:password_confirmation => params[:password_confirmation])
+		if @user.save
+			session[:user_id] = @user.id
+			redirect to('/')
+		else
+			flash.now[:errors] = @user.errors.full_messages
+			erb:"users/new"
+		end
 	end
