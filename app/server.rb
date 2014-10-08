59a6114b67783a3945e2f87f458202cd6e48c333
Zeeshan Rasool <zrasool88@gmail.com>
2014-10-08 11:30:58 +0100

    signing in works

--- app/server.rb
+++ app/server.rb
@@ -35,14 +35,30 @@ use Rack::Flash
 		erb :"users/new"
 	end
 
+		get '/sessions/new' do
+		erb :"sessions/new"
+	end
+
+	post '/sessions' do
+		email, password = params[:email], params[:password]
+		user = User.authenticate(email, password)
+		if user
+			session[:user_id] = user.id
+			redirect to('/')
+		else
+			flash[:errors] = ["The email or password is incorrect"]
+			erb :"sessions/new"
+		end
+	end
+
 	post '/users' do
-		@user = User.new(:email => params[:email], :password => params[:password], 
-						:password_confirmation => params[:password_confirmation])
+		@user = User.new(:email => params[:email], :password => params[:password],
+						 :password_confirmation => params[:password_confirmation])
 		if @user.save
 			session[:user_id] = @user.id
 			redirect to('/')
 		else
 			flash.now[:errors] = @user.errors.full_messages
-			erb:"users/new"
+			erb :"users/new"
 		end
 	end
